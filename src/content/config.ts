4db3e456f27ba84d6655b167711effd9a08a303f
Franklin Shera <fshera96@gmail.com>
2023-05-24 19:10:40 +0300

    add frontmatter cms

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,26 @@
-import { z, defineCollection } from "astro:content";
+import { z, defineCollection, reference } from "astro:content";
 
-const docsCollection = defineCollection({
+const docs = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
     tags: z.array(z.string()),
     framework: z.string(),
     image: z.string().optional(),
+    draft: z.boolean(),
+    publishDate: z.date(),
+    author: reference("authors"),
+  }),
+});
+
+const authors = defineCollection({
+  type: "data",
+  schema: z.object({
+    name: z.string(),
   }),
 });
 
 export const collections = {
-  docs: docsCollection,
+  docs,
+  authors,
 };
---
+++ src/content/docs/2023-05-24-second-post-on-frontmatter.md
@@ -0,0 +1,15 @@
+---
+title: Second post on Frontmatter
+slug: post-frontmatter
+publishDate: 2023-05-24T16:05:04.107Z
+image: ""
+framework: vue
+draft: true
+tags:
+  - vue
+author: shera
+---
+
+# Second post on Frontmatter
+
+Scond post

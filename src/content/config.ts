5cabe5b854cc4cfdfb70e3a20d354afc94147739
Franklin Shera <fshera96@gmail.com>
2023-05-24 17:19:03 +0300

    content collections

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { z, defineCollection } from "astro:content";
+
+const docsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    framework: z.string(),
+    image: z.string().optional(),
+  }),
+});
+
+export const collections = {
+  docs: docsCollection,
+};

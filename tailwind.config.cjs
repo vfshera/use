b28728f687334f7bd1b8893129061dadbebb5b14
Franklin Shera <fshera96@gmail.com>
2023-05-26 23:47:59 +0300

    colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: { 1: "#000001ff", 2: "#000708ff" },
+        silver: "#BFC5C8ff",
+        "eerie-black": "#202426ff",
+        gray: "#727D80ff",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
